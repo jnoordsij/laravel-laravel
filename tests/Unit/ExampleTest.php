2cee6679fd8fcefcba4087b246c5de7c0d364bb1
Jesper Noordsij <jesper@sslleiden.nl>
2023-04-17 16:05:51 +0200

    Example of silenced PHPUnit errors

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -10,6 +11,17 @@ class ExampleTest extends TestCase
      * A basic test example.
      */
     public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public static function provider()
+    {
+        yield from [];
+    }
+
+    #[DataProvider('provider')]
+    public function test_with_provider()
     {
         $this->assertTrue(true);
     }
