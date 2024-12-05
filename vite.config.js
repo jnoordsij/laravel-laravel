fa175a3a0bed56457b21697f35c6c78be109b25d
Jesper Noordsij <jesper@sslleiden.nl>
2024-12-05 17:58:36 +0100

    Add vite-plugin-webfont-dl

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import webfontDownload from 'vite-plugin-webfont-dl';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,9 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        webfontDownload([
+            'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap',
+            'https://fonts.googleapis.com/css2?family=Fira+Code&display=swap'
+        ]),
     ],
 });
