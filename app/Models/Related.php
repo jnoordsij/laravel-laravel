d9e8cbca60cd59b9e1b32b77ace11e016fa260e8
Jesper Noordsij <jesper@sslleiden.nl>
2025-02-17 12:53:13 +0100

    Add alternative solution

--- app/Models/Related.php
+++ app/Models/Related.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class Related extends Model
 {
@@ -26,6 +27,18 @@ class Related extends Model
         $user = $query->getModel()->user();
 
         return $query->whereHas($user, fn ($q) => $q->search($search));
+    }
+
+    /**
+     * @param  Builder<static>  $query
+     * @return Builder<static>
+     */
+    public function scopeSearchAlternative(Builder $query, ?string $search = null): Builder
+    {
+        return $query->whereHas(
+            Relation::noConstraints(fn () => $query->getModel()->user()),
+            fn ($q) => $q->search($search),
+        );
     }
 
     /**
