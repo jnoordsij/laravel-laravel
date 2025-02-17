4a1d873b9afcaffe08cd522d7950cbc2e593ade2
Jesper Noordsij <jesper@sslleiden.nl>
2025-02-17 12:53:03 +0100

    Add mwe to illustrate difference in runtime

---
+++ app/Models/Related.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Related extends Model
+{
+    /**
+     * @param  Builder<static>  $query
+     * @return Builder<static>
+     */
+    public function scopeSearchOriginal(Builder $query, ?string $search = null): Builder
+    {
+        return $query->whereHas('user', fn ($q) => $q->search($search));
+    }
+
+    /**
+     * @param  Builder<static>  $query
+     * @return Builder<static>
+     */
+    public function scopeSearch(Builder $query, ?string $search = null): Builder
+    {
+        $user = $query->getModel()->user();
+
+        return $query->whereHas($user, fn ($q) => $q->search($search));
+    }
+
+    /**
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
